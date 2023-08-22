ee7baccb2d65c29f42388aa482748901b827c32e
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-08-22 15:09:47 +0900

    Update InventoryController.java

--- inventory/src/main/java/labshopmonolith/infra/InventoryController.java
+++ inventory/src/main/java/labshopmonolith/infra/InventoryController.java
@@ -21,7 +21,7 @@ public class InventoryController {
     InventoryRepository inventoryRepository;
 
     @RequestMapping(
-        value = "inventories/{id}/d",
+        value = "inventories/{id}/decreasestock",
         method = RequestMethod.PUT,
         produces = "application/json;charset=UTF-8"
     )
