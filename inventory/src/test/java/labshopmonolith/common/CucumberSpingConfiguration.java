7541fb6491b33ac216d380383966675b343967fc
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-08-30 09:09:36 +0900

    Delete inventory/src/test directory

--- inventory/src/test/java/labshopmonolith/common/CucumberSpingConfiguration.java
+++
@@ -1,9 +0,0 @@
-package labshopmonolith.common;
-
-import io.cucumber.spring.CucumberContextConfiguration;
-import labshopmonolith.InventoryApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@CucumberContextConfiguration
-@SpringBootTest(classes = { InventoryApplication.class })
-public class CucumberSpingConfiguration {}
