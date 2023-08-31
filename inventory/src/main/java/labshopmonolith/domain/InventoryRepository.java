d3d2adc90b8f35558878ee758dba0f11482d3d0f
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-08-31 16:09:37 +0900

    Update InventoryRepository.java

--- inventory/src/main/java/labshopmonolith/domain/InventoryRepository.java
+++ inventory/src/main/java/labshopmonolith/domain/InventoryRepository.java
@@ -4,7 +4,6 @@ import labshopmonolith.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-//<<< PoEAA / Repository
 @RepositoryRestResource(
     collectionResourceRel = "inventories",
     path = "inventories"
