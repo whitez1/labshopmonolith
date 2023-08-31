2613ab0ae05f624a02b997a733000c65d1877d6d
KimKyusoo <123912988+kyusooK@users.noreply.github.com>
2023-08-31 16:07:57 +0900

    Update OrderPlaced.java

--- monolith/src/main/java/labshopmonolith/domain/OrderPlaced.java
+++ monolith/src/main/java/labshopmonolith/domain/OrderPlaced.java
@@ -6,7 +6,6 @@ import labshopmonolith.domain.*;
 import labshopmonolith.infra.AbstractEvent;
 import lombok.*;
 
-//<<< DDD / Domain Event
 @Data
 @ToString
 public class OrderPlaced extends AbstractEvent {
@@ -24,4 +23,3 @@ public class OrderPlaced extends AbstractEvent {
         super();
     }
 }
-//>>> DDD / Domain Event
