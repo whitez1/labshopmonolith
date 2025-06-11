6971b47baa3adfbe562ed59089d927933150d0dd
Heeji Jang <heeji0502@gmail.com>
2025-06-11 06:08:58 +0000

    stock decrease 로직 추가

--- inventory/src/main/java/labshopmonolith/domain/Inventory.java
+++ inventory/src/main/java/labshopmonolith/domain/Inventory.java
@@ -29,7 +29,7 @@ public class Inventory {
 
     public void decreaseStock(DecreaseStockCommand decreaseStockCommand) {
         //implement business logic here:
-
+        setStock(getStock() - decreaseStockCommand.getQty().longValue());
     }
 
 }
